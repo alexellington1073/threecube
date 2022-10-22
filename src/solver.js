7b14300832c7bb89ed5ffe5bcf35c1d96a927014
alexellington1073 <alexellington1073@yahoo.com>
2022-10-22 11:50:40 -0400

    buildin

--- src/solver.js
+++ src/solver.js
@@ -21,29 +21,40 @@ export function solve(stickers) {
 
 
     //needs testing
-    function getPiece (stickers, posArr) {
+    //returns sticker objects
+    function getPiece (stickers, posArr, name) {
         let piece = []
         let adjustedPos = posArr
 
         for (let i = 0; i < posArr.length; i++) {
             adjustedPos[i] = Math.trunc(posArr[i])
+            if (adjustedPos[i] == -0) adjustedPos[i] = 0
         }
 
         for (let i = 0; i < stickers.length; i++) {
+            if (stickers[i].name === name) continue
             let currentStickerPos = []
-            for (let j = 0; j < stickers.position.toArray().length; j++) {
+            for (let j = 0; j < stickers[i].position.toArray().length; j++) {
                 currentStickerPos[j] = Math.trunc(stickers[i].position.toArray()[j])
+                if (currentStickerPos[j] == -0) currentStickerPos[j] = 0
             }
+            console.log(currentStickerPos,adjustedPos)
 
             for (let j = 0; j < adjustedPos.length; j++) {
                 if (adjustedPos[j] != currentStickerPos[j]) break
-                else if (i == adjustedPos.length) piece.push(stickers[i])
+                else if (i == adjustedPos.length - 1) piece.push(stickers[i])
             }
 
-            if (piece.length + getZeroCount(posArr) === 3) return piece
+            if (piece.length + getZeroCount(posArr) === 2) return piece
 
         }
 
+    }
+
+    function getTargetCenter(stickers, name) {
+        for (let i  = 0; i < stickers.length; i++) {
+            if (getZeroCount(stickers[i]) == 2 && stickers[i].getName() == name) return stickers[i]
+        }
     }
 
     function getZeroCount(positionArray) {
@@ -65,12 +76,30 @@ export function solve(stickers) {
             const name = stickers[i].name
 
             if (getZeroCount(posArr) == 1 && name === 'white') {
-                edgeStickers.push(i)
+                edgeStickers.push(stickers[i])
             }
         }
 
+
+        //while loop? recursion???
         for (let i = 0; i < edgeStickers.length; i++) {
-            const posArr = stickers[edgeStickers[i]].position.toArray()
+            const posArr = edgeStickers[i].position.toArray()
+            let sideColor = getPiece(stickers, posArr, edgeStickers[i].name)
+            console.log(sideColor)
+
+            //piece mis-oriented top case
+            if (posArr[1] == 1) {
+                //check if white edges directly underneath and side underneath and in correct position
+                //rotate current piece to side, then top and turn top
+                //undo up to 2 rotations if first check was true
+            }
+            //piece mis-oriented middle case
+            if (posArr[1] == 0) {
+                //check if white edge underneath and in correct position
+                //rotate current piece to top and turn top
+                //undo first rotation if first check was true
+
+            }
 
             //piece on bottom case
             if (posArr[1] == -1.5) {
@@ -83,16 +112,15 @@ export function solve(stickers) {
             //piece on top case
             if (posArr[1] == 1.5) {
                 //rotate to correct side, rotate down
+                let targetCenterPos = getTargetCenter(stickers,edgeStickers[i].name)
+
+                // if (  )
+
 
 
             }
 
-            //piece mis-oriented case
-            else {
-                //check if correctly placed piece underneath.
-                //rotate up, turn up
-                // rotate back down if first check was true
-            }
+
 
 
         }
